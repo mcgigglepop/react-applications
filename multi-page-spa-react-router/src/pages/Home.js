eeeea168bc83a0516e50b11d92cbc44b8ea63450
cristopher ontiveros <christoph.alano@gmail.com>
2024-02-04 17:16:30 -0600

    base

---
+++ multi-page-spa-react-router/src/pages/Home.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <h1>my home page</h1>
+}
+
+export default HomePage;
---
+++ multi-page-spa-react-router/src/pages/Products.js
@@ -0,0 +1,5 @@
+function ProductsPage() {
+    return <h1>the products page</h1>
+}
+
+export default ProductsPage;
