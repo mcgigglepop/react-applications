4a00ea86f4be5254f15f9c1a762b1ce8ccd58eb3
cristopher ontiveros <christoph.alano@gmail.com>
2024-02-04 17:31:22 -0600

    added link

--- multi-page-spa-react-router/src/pages/Home.js
+++ multi-page-spa-react-router/src/pages/Home.js
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+
 function HomePage() {
-    return <h1>my home page</h1>
+    return (
+        <>
+            <h1>my home page</h1>
+            <p>
+                <Link to="/products">the list of products</Link>
+            </p>
+        </>
+        
+    );
 }
 
 export default HomePage;
