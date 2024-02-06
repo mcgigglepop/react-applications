9c0154d2b352e7beea8f86a7cfbc82e1e821e2af
cristopher ontiveros <christoph.alano@gmail.com>
2024-02-05 20:43:32 -0600

    fix typo

--- advanced-state-management/src/App.jsx
+++ advanced-state-management/src/App.jsx
@@ -70,7 +70,7 @@ function App() {
 
   const ctxValue = {
     items: shoppingCart.items,
-    addIItemToCart: handleAddItemToCart,
+    addItemToCart: handleAddItemToCart,
     updateItemQuantity: handleUpdateCartItemQuantity,
   }
 
