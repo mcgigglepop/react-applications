41caa6b164329aae697e9d107a245e430ae4aed8
cristopher ontiveros <christoph.alano@gmail.com>
2024-02-10 04:24:56 -0600

    outsource get available places

--- database/src/components/AvailablePlaces.jsx
+++ database/src/components/AvailablePlaces.jsx
@@ -4,6 +4,8 @@ import Error from './Error.jsx';
 
 import { sortPlacesByDistance } from '../loc.js';
 
+import { fetchAvailablePlaces } from '../http.js';
+
 const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
@@ -16,15 +18,11 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await  fetch('http://localhost:3000/places');
-        const resData = await response.json();
         
-        if(!response.ok){
-          throw new Error('Failed to fetch places');
-        }
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
+          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
           setAvailablePlaces(sortedPlaces);
           setIsFetching(false);
         });
