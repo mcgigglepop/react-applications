10ab767cf4f3fb7658bdb4e1a52738071e0147b1
cristopher ontiveros <christoph.alano@gmail.com>
2024-02-11 02:00:01 -0600

    map out the error messages

--- authentication/frontend/src/pages/Authentication.js
+++ authentication/frontend/src/pages/Authentication.js
@@ -20,7 +20,7 @@ export async function action({ request }) {
     password: data.get('password'),
   };
 
-  const response = await fetch('http://localhost:8000/' + mode, {
+  const response = await fetch('http://localhost:8080/' + mode, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
